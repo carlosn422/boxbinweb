fb048195e6ac5828b616439c708c06b60c5e9052
carlosn422 <76015730+carlosn422@users.noreply.github.com>
2025-09-02 12:21:23 -0500

    Updated cancel button locatioj

--- src/pages/billing/ActiveSubscriptionPage.tsx
+++ src/pages/billing/ActiveSubscriptionPage.tsx
@@ -336,7 +336,7 @@ export default function ActiveSubscriptionPage() {
 
         <Card className="border border-gray-200 shadow-lg">
           <CardHeader className="text-center relative">
-            <div className="absolute top-4 right-4">
+            <div className="relative">
               <Button
                 disabled={redirecting}
                 onClick={() => setShowCancelDialog(true)}
